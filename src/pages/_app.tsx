d8ec88b45e1edba42ee144e63e93846f274ac2f6
Alexlinsy <alexlinsy@live.com>
2023-04-05 23:01:48 -0600

    Connect local graphql server with urql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,20 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
+import { Client, cacheExchange, fetchExchange, Provider } from 'urql';
+
+const client = new Client({
+  url: 'http://localhost:4000/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,15 +3,30 @@ import { Form, Formik } from "formik";
 import { Box, Button } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `mutation Register($username: String!, $password: String!) {
+    login(options: {username: $username, password: $password}) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }`
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [,register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => register(values)}
       >
         {({ isSubmitting }) => (
           <Form>
